0d8e35cbe2d0ad842ecc776dee98e5e19fdc22cb
chalith <desaman.chalith@gmail.com>
2024-01-24 14:49:09 +0530

    Changed file location

--- sashimono/patches/update-message-board.sh
+++ sashimono/patches/update-message-board.sh
@@ -9,7 +9,7 @@ export MB_XRPL_SERVICE="sashimono-mb-xrpl"
 export MB_XRPL_USER="sashimbxrpl"
 export MB_XRPL_BIN=$SASHIMONO_BIN/mb-xrpl
 
-curl https://raw.githubusercontent.com/$repo_owner/$repo_name/main/sashimono/pathches/resources/mb-xrpl/$file > $file && echo "$file update failed"
+curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/main/sashimono/pathches/resources/mb-xrpl/$file" > "$MB_XRPL_BIN/$file" && echo "$file update failed"
 
 local mb_user_id=$(id -u "$MB_XRPL_USER")
 local mb_user_runtime_dir="/run/user/$mb_user_id"
