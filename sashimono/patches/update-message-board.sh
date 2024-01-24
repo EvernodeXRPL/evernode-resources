93bde8d1b0245fd91b92453316e43a8cd157117b
chalith <desaman.chalith@gmail.com>
2024-01-24 15:13:36 +0530

    Updated file path

--- sashimono/patches/update-message-board.sh
+++ sashimono/patches/update-message-board.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ "$EUID" -ne 0 ] && echo "Please run with root privileges (sudo)." && exit 1
+
 repo_owner="EvernodeXRPL"
 repo_name="evernode-resources"
 file="index.js"
@@ -9,9 +11,14 @@ export MB_XRPL_SERVICE="sashimono-mb-xrpl"
 export MB_XRPL_USER="sashimbxrpl"
 export MB_XRPL_BIN=$SASHIMONO_BIN/mb-xrpl
 
-curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/main/sashimono/pathches/resources/mb-xrpl/$file" > "$MB_XRPL_BIN/$file" && echo "$file update failed"
+mv "$MB_XRPL_BIN/$file" "$MB_XRPL_BIN/$file.bk"
 
-local mb_user_id=$(id -u "$MB_XRPL_USER")
-local mb_user_runtime_dir="/run/user/$mb_user_id"
+curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/patches/sashimono/patches/resources/mb-xrpl/$file" -o "$MB_XRPL_BIN/$file" &&
+    echo "$file update failed" &&
+    cp "$MB_XRPL_BIN/$file.bk" "$MB_XRPL_BIN/$file" &&
+    exit 1
+
+mb_user_id=$(id -u "$MB_XRPL_USER")
+mb_user_runtime_dir="/run/user/$mb_user_id"
 
 sudo -u "$MB_XRPL_USER" XDG_RUNTIME_DIR="$mb_user_runtime_dir" systemctl --user restart $MB_XRPL_SERVICE
