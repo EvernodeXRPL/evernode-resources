20d9ad1b0b6f6becccaddc0369a08ab1297ae9f5
chalith <desaman.chalith@gmail.com>
2024-01-24 15:19:12 +0530

    Added logs

--- sashimono/patches/update-message-board.sh
+++ sashimono/patches/update-message-board.sh
@@ -11,14 +11,22 @@ export MB_XRPL_SERVICE="sashimono-mb-xrpl"
 export MB_XRPL_USER="sashimbxrpl"
 export MB_XRPL_BIN=$SASHIMONO_BIN/mb-xrpl
 
+echo "Backing up the files.."
+
 mv "$MB_XRPL_BIN/$file" "$MB_XRPL_BIN/$file.bk"
 
-! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/patches/sashimono/patches/resources/mb-xrpl/$file" -o "$MB_XRPL_BIN/$file" &&
-    echo "$file update failed" &&
-    cp "$MB_XRPL_BIN/$file.bk" "$MB_XRPL_BIN/$file" &&
+echo "Updating the files.."
+
+if [ ! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/patches/sashimono/patches/resources/mb-xrpl/$file" -o "$MB_XRPL_BIN/$file" ]; then
+    echo "Update failed. Restoring.."
+    ! cp "$MB_XRPL_BIN/$file.bk" "$MB_XRPL_BIN/$file" && echo "Restoring failed." && exit 1
+    echo "Restored."
     exit 1
+fi
+
+echo "Restarting the message board.."
 
 mb_user_id=$(id -u "$MB_XRPL_USER")
 mb_user_runtime_dir="/run/user/$mb_user_id"
 
-sudo -u "$MB_XRPL_USER" XDG_RUNTIME_DIR="$mb_user_runtime_dir" systemctl --user restart $MB_XRPL_SERVICE
+! sudo -u "$MB_XRPL_USER" XDG_RUNTIME_DIR="$mb_user_runtime_dir" systemctl --user restart $MB_XRPL_SERVICE && echo "Message board restart failed."
